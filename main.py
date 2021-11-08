89ee1c196d114e4eaab6d635ccd022efa7e81687
Indrani Sen <sen.indrani0608@gmail.com>
2021-11-08 11:22:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-pip install requests
-
+#import the neccessary package
 import requests
 
+#Take city name as user  input
 city=input("Enter your city name")
 
 print(city)
 
 print("Displaying Weather Report for: " + city)
 
+#Fetch the weather deatils from wttr.in
 url='https://wttr.in/{}'.format(city)
 
+#Store the result in result variable
 result=requests.get(url)
 
+#display the result
 print(result.text)
 
